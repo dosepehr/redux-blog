5c37e12741b85fc2b49e78981bb89d4db43d5974
dosepehr <dosepehr@gmail.com>
2023-04-20 00:36:52 +0430

    make blogSlice

---
+++ src/reducers/blogSlice.js
@@ -0,0 +1,13 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+const blogSlice = createSlice({
+    name: 'blogs',
+    initialState: [],
+    reducers: {
+        
+    }
+})
+
+
+export default blogSlice.reducer
