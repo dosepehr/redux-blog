1ee3e1b22f07eb281e1e8e4cab9d924571c9c583
dosepehr <dosepehr@gmail.com>
2023-04-19 22:34:47 +0430

    add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+// routes
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes/routes';
+// tailwindcss
 import '../index.css';
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={routes} />
     </StrictMode>
 );
 
