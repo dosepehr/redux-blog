66b1fcd78841d32ff098e3e91708321cca6ef195
dosepehr <dosepehr@gmail.com>
2023-04-21 21:50:08 +0430

    add showing spinner logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { fetchBlogs } from './reducers/blogsSlice';
 import { fetchUsers } from './reducers/usersSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 function App() {
+    const [loading, setLoading] = useState(false);
     const dispatch = useDispatch();
+    const blogStatus = useSelector((state) => state.blogs.status);
+    const usersStatus = useSelector((state) => state.users.status);
     useEffect(() => {
-        dispatch(fetchBlogs());
-        dispatch(fetchUsers());
-    }, []);
+        if (blogStatus == 'idle') {
+            dispatch(fetchBlogs());
+        }
+        if (usersStatus == 'idle') {
+            dispatch(fetchUsers());
+        }
+        if (blogStatus == 'loading' || usersStatus == 'loading') {
+            setLoading(true);
+        } else {
+            setLoading(false);
+        }
+    }, [blogStatus, usersStatus, dispatch]);
     return (
         <div className='app'>
             <Navbar />
-            <Outlet />
+            {loading ? <p>it's loading</p> : <Outlet />}
         </div>
     );
 }
