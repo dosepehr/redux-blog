c72cfaaff05eaf65f53b11c6de382ba9d1abb0df
dosepehr <dosepehr@gmail.com>
2023-04-22 03:13:03 +0430

    edit styles

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { fetchUsers, selectAllUsers } from '../reducers/usersSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { selectAllUsers } from '../reducers/usersSlice';
+import { useSelector } from 'react-redux';
 const UsersList = () => {
     const users = useSelector((state) => selectAllUsers(state));
 
     return (
         <div>
-            {users.length > 0 &&
-                users.map((user) => <p key={user.id}>{user.name}</p>)}
+            <p>لیست نویسندگان</p>
+            <ul className='space-y-2 mt-3 w-fit'>
+                {users.length > 0 &&
+                    users.map((user) => <li key={user.id} className='bg-sky-200'>{user.name}</li>)}
+            </ul>
         </div>
     );
 };
