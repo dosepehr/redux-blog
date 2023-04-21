7fe51a9e9b66df602026a1ce0764a3c835dcffee
dosepehr <dosepehr@gmail.com>
2023-04-21 04:46:31 +0430

    get users data from server

--- src/reducers/usersSlice.js
+++ src/reducers/usersSlice.js
@@ -1,16 +1,24 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { getAllUsers } from '../services';
+
+export const fetchUsers = createAsyncThunk('/users/fetchUsers', async () => {
+    const { data } = await getAllUsers();
+    return data;
+});
+
 const usersSlice = createSlice({
     name: 'users',
     initialState: {},
     reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            return action.payload;
+        });
+    },
 });
 
-export const fetchUsers = createAsyncThunk('/users/fetchUsers', async () => {
-    const {data} = await getAllUsers();
-    return data;
-});
-
-export const {} = usersSlice.actions;
+export const selectAllUsers = (state) => state.users;
+export const selectUsersById = (state, userId) =>
+    state.users.find((user) => user.id === userId);
 
 export default usersSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import blogsReducer from '../reducers/blogsSlice';
 import usersReducer from '../reducers/usersSlice';
-
+import { fetchUsers } from '../reducers/usersSlice';
 export const store = configureStore({
     reducer: {
         blogs: blogsReducer,
         users: usersReducer,
     },
 });
+
+// fetch all users from api
+store.dispatch(fetchUsers());
